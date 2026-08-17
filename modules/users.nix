04136e07647f01ba70bf5ee2f21fdccffe78d798
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-17 12:17:06 +0530

    Set initial password reejitbiswas for rebiz and root accounts

--- modules/users.nix
+++ modules/users.nix
@@ -5,6 +5,7 @@
   users.users.rebiz = {
     isNormalUser = true;
     description = "rebiz";
+    initialPassword = "reejitbiswas"; # Initial password for user rebiz
     extraGroups = [ "wheel" "networkmanager" "docker" "storage" "video" "audio" ];
     shell = pkgs.bashInteractive;
     openssh.authorizedKeys.keys = [
@@ -13,11 +14,14 @@
     ];
   };
 
-  # Root SSH authorized keys for deployment and management
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9CvwTALuQuiHJlkXTs2U5SKMhiu/lag3jQsbBIyHCl guardiansofspartax@gmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVKdJ2d/APmJOYmjZtggs39BmS1sF96wJnwoEc0ErQQ guardiansofspartax@gmail.com"
-  ];
+  # Root user initial password and SSH keys
+  users.users.root = {
+    initialPassword = "reejitbiswas"; # Initial password for root
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9CvwTALuQuiHJlkXTs2U5SKMhiu/lag3jQsbBIyHCl guardiansofspartax@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVKdJ2d/APmJOYmjZtggs39BmS1sF96wJnwoEc0ErQQ guardiansofspartax@gmail.com"
+    ];
+  };
 
   # Passwordless sudo for wheel group users
   security.sudo.wheelNeedsPassword = false;
