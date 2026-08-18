5152a8025e3d7ef3c3ca3617abc2de0a0d7cde13
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-18 13:35:39 +0530

    feat(user): switch shell to fish and enable sudo-rs

--- modules/system/core/user.nix
+++ modules/system/core/user.nix
@@ -6,11 +6,13 @@ _: {
   }: {
     users.mutableUsers = false;
 
+    programs.fish.enable = true;
+
     users.users.rebiz = {
       isNormalUser = true;
       hashedPasswordFile = config.sops.secrets.user_password.path;
       extraGroups = ["wheel" "networkmanager" "docker" "storage" "video" "render" "audio"];
-      shell = pkgs.bashInteractive;
+      shell = pkgs.fish;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9CvwTALuQuiHJlkXTs2U5SKMhiu/lag3jQsbBIyHCl guardiansofspartax@gmail.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINVKdJ2d/APmJOYmjZtggs39BmS1sF96wJnwoEc0ErQQ guardiansofspartax@gmail.com"
@@ -19,7 +21,8 @@ _: {
 
     users.users.root.hashedPasswordFile = config.sops.secrets.user_password.path;
 
-    security.sudo = {
+    security.sudo-rs = {
+      enable = true;
       wheelNeedsPassword = false;
       execWheelOnly = true;
     };
